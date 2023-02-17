87cd11c284efd48a81a8c1f68120eed2c0800bba
Alex <lxkrmr@icloud.com>
2023-02-17 14:27:13 +0100

    ✨  add AccordionPage

--- src/App.js
+++ src/App.js
@@ -1,28 +1,5 @@
-import Accordion from './components/Accordion';
-
 function App() {
-  const items = [
-    {
-      id: 'id-1',
-      label: 'This is the first label with a bit more text.',
-      content:
-        'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At',
-    },
-    {
-      id: 'id-2',
-      label: 'This is a second label!',
-      content:
-        'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At',
-    },
-    {
-      id: 'id-3',
-      label: 'Finally a third label.',
-      content:
-        'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At',
-    },
-  ];
-
-  return <Accordion items={items} />;
+  return <h1>Another start</h1>;
 }
 
 export default App;
---
+++ src/pages/AccordionPage.js
@@ -0,0 +1,28 @@
+import Accordion from '../components/Accordion';
+
+function AccordionPage() {
+  const items = [
+    {
+      id: 'id-1',
+      label: 'This is the first label with a bit more text.',
+      content:
+        'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At',
+    },
+    {
+      id: 'id-2',
+      label: 'This is a second label!',
+      content:
+        'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At',
+    },
+    {
+      id: 'id-3',
+      label: 'Finally a third label.',
+      content:
+        'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At',
+    },
+  ];
+
+  return <Accordion items={items} />;
+}
+
+export default AccordionPage;
