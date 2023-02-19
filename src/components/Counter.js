4eead6970ffbf03720b9025d5c4fb9314db7a694
Alex <lxkrmr@icloud.com>
2023-02-19 15:56:42 +0100

    ♻️ use useReducer instead of useState

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,24 +1,57 @@
 import Button from './Button';
-import { useState } from 'react';
+import { useReducer } from 'react';
 import Panel from './Panel';
 
+const reducer = (state, action) => {
+  if (action.type === 'counter-increment') {
+    return {
+      ...state,
+      counter: state.counter + (action.payload ?? 1),
+    };
+  }
+
+  if (action.type === 'counter-decrement') {
+    return {
+      ...state,
+      counter: state.counter - (action.payload ?? 1),
+    };
+  }
+
+  if (action.type === 'add-change') {
+    return {
+      ...state,
+      add: parseInt(action.payload) ?? 0,
+    };
+  }
+
+  console.error(`Unkown action.type: ${action.type}`);
+  return state;
+};
+
 function Counter({ initialCount }) {
-  const [counter, setCounter] = useState(initialCount ?? 0);
-  const [add, setAdd] = useState(0);
+  const [state, dispatch] = useReducer(reducer, {
+    counter: initialCount ?? 0,
+    add: 0,
+  });
 
-  const increment = () => setCounter((current) => current + 1);
-  const decrement = () => setCounter((current) => current - 1);
+  const increment = () => dispatch({ type: 'counter-increment' });
+  const decrement = () => dispatch({ type: 'counter-decrement' });
 
-  const handleAddChange = (event) => setAdd(parseInt(event.target.value) || 0);
+  const handleAddChange = (event) => {
+    dispatch({
+      type: 'add-change',
+      payload: event.target.value,
+    });
+  };
   const handleAddSumbit = (event) => {
     event.preventDefault();
-    setCounter((current) => current + add);
-    setAdd(0);
+    dispatch({ type: 'counter-increment', payload: state.add });
+    dispatch({ type: 'add-change', payload: 0 });
   };
 
   return (
     <Panel className="m-3">
-      <h1 className="text-lg">Count: {counter}</h1>
+      <h1 className="text-lg">Count: {state.counter}</h1>
       <div className="flex flex-row">
         <Button onClick={increment} outline>
           increment
@@ -31,7 +64,7 @@ function Counter({ initialCount }) {
       <form onSubmit={handleAddSumbit}>
         <label>Add a lot!</label>
         <input
-          value={add || ''}
+          value={state.add || ''}
           onChange={handleAddChange}
           type="number"
           className="p-1 m-3 bg-gray-50 border border-gray-300"
